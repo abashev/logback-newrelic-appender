c268be698b41020580cff36d6ff62302701d1952
Alexey Abashev <alexey@abashev.ru>
2014-08-19 20:56:48 +0400

    Freeze for version 1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 logback-newrelic-appender
 =========================
 
-Logback appender for sending error messages directly to NewRelic monitoring
+Logback appender for sending error messages directly to NewRelic monitoring. So you don't need to add NewRelic.noticeError() everywhere but just standard log.error() from slf4j will be enough.
+
+
+How to use it inside your project
+---
+
+Add new repository into your Maven configuration
+
+    <repositories>
+        <repository>
+            <id>logback-newrelic-appender-repository</id>
+            <url>http://dl.bintray.com/abashev/logback-newrelic-appender</url>
+            <snapshots>
+                <enabled>false</enabled>
+            </snapshots>
+        </repository>
+    </repositories>
+  
+Add new dependency into dependencies list
+
+    <dependency>
+        <groupId>com.github.abashev.logback-newrelic-appender</groupId>
+        <artifactId>logback-newrelic-appender</artifactId>
+        <version>1.0.0</version>
+    </dependency>
+
+And from now you could use it inside your logback configuration file like this
+
+    <appender name="newrelic" class="com.github.abashev.logback.NewRelicAppender">
+        <filter class="ch.qos.logback.classic.filter.ThresholdFilter">
+            <level>ERROR</level>
+        </filter>
+    </appender>
+
+    <root level="info">
+        <appender-ref ref="newrelic" />
+    </root>
+
+*Note: It is better to apply messages threshold filter because too many NewRelic messages could break down performance of your application.*
+
+
--- src/main/java/com/github/abashev/logback/NewRelicAppender.java
+++ src/main/java/com/github/abashev/logback/NewRelicAppender.java
@@ -5,6 +5,9 @@ import ch.qos.logback.classic.spi.IThrowableProxy;
 import ch.qos.logback.classic.spi.ThrowableProxy;
 import ch.qos.logback.core.AppenderBase;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.newrelic.api.agent.NewRelic.noticeError;
 
 /**
@@ -13,20 +16,29 @@ import static com.newrelic.api.agent.NewRelic.noticeError;
  * @author <A href="mailto:alexey at abashev dot ru">Alexey Abashev</A>
  */
 public class NewRelicAppender extends AppenderBase<ILoggingEvent> {
+    private static final String MESSAGE_PARAM = "message";
+
     @Override
-    protected void append(ILoggingEvent eventObject) {
-        final IThrowableProxy throwableProxy = eventObject.getThrowableProxy();
+    protected void append(ILoggingEvent event) {
+        final IThrowableProxy throwableProxy = event.getThrowableProxy();
+        final Map<String, String> mdc = event.getMDCPropertyMap();
 
         if ((throwableProxy != null) && (throwableProxy instanceof ThrowableProxy)) {
             ThrowableProxy proxy = (ThrowableProxy) throwableProxy;
 
             if (proxy.getThrowable() != null) {
-                noticeError(proxy.getThrowable(), eventObject.getMDCPropertyMap());
+                Map<String, String> params = new HashMap<>(mdc);
+
+                if (!mdc.containsKey(MESSAGE_PARAM)) {
+                    params.put(MESSAGE_PARAM, event.getFormattedMessage());
+                }
+
+                noticeError(proxy.getThrowable(), params);
 
                 return;
             }
         }
 
-        noticeError(eventObject.getFormattedMessage(), eventObject.getMDCPropertyMap());
+        noticeError(event.getFormattedMessage(), mdc);
     }
 }
